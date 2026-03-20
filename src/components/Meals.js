e68b96217f2f875d8a7213922b677f82734e347e
kris-v <kristjan.kraak@voco.ee>
2026-03-20 09:35:45 +0000

    Ülesanne 3
    Võtis kaua CORSI probleemi pärast codespace'is

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,4 +1,23 @@
+import { useEffect } from "react";
+
 const Meals = () => {
+    useEffect(() => {
+        const fetchMeals = async () => {
+            try {
+                const apiUrl = `${window.location.origin
+                    .replace("-3000.", "-3001.")
+                    .replace(":3000", ":3001")}/meals`;
+                const response = await fetch(apiUrl);
+                const meals = await response.json();
+                console.log(meals);
+            } catch (err) {
+                console.error("Fetching meals failed", err);
+            }
+        };
+
+        fetchMeals();
+    }, []);
+
     return (
         <ul id="meals">
             { 
