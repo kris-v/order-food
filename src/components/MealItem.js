96e7d2d89fd99ae2cb0ee2a4e2cb3f54cd870b48
kris-v <kristjan.kraak@voco.ee>
2026-03-20 09:46:40 +0000

    Ülesanne 5

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -1,5 +1,7 @@
 const MealItem = (props) => {
     const dish = props.meal;
+    const FormattedPrice = new Intl.NumberFormat("et-EE", { style: "currency", currency: "EUR" })
+        .format(Number(dish.price));
 
     return (
         <li className="meal-item">
@@ -7,7 +9,7 @@ const MealItem = (props) => {
                 <img src={require(`../assets/${dish.image}`)} alt={dish.name}/>
                 <div>
                     <h3>{dish.name}</h3>
-                    <p className="meal-item-price">${dish.price}</p>
+                    <p className="meal-item-price">{FormattedPrice}</p>
                     <p className="meal-item-description">{dish.description}</p>
                 </div>
                 <p className="meal-item-actions">
